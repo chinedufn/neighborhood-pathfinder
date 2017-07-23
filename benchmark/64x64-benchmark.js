a660afdc299bc0da58634539a057ef61c86ca18f
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2017-07-23 18:28:29 +0900

    Add l1-path-finder to benchmark
    l1-path-finder is over 100x faster right now

--- benchmark/64x64-benchmark.js
+++ benchmark/64x64-benchmark.js
@@ -1,13 +1,36 @@
 var weightedPathfinder = require('../')
+
+var ndarray = require('ndarray')
+var createPlanner = require('l1-path-finder')
+
 var grid = require('./64x64_fixture.js')
 
-var opts = {
-  start: [11, 11],
-  end: [63, 63],
-  grid: grid,
-  gridWidth: 64,
-  allowDiagonal: true,
-  dontCrossBlockedTiles: true
-}
-var path = weightedPathfinder.findPath(opts)
-console.log(path)
+var bench = require('nanobench')
+
+var numTimesToExecute = 1000
+bench(`neighborhood-pathfinder [11, 11] to [63, 63] with very few obstacles ${numTimesToExecute} times`, function (b) {
+  for (var i = 0; i < numTimesToExecute; i++) {
+    var opts = {
+      start: [11, 11],
+      end: [63, 63],
+      grid: grid,
+      gridWidth: 64,
+      allowDiagonal: true,
+      dontCrossBlockedTiles: true
+    }
+    weightedPathfinder.findPath(opts)
+  }
+
+  b.end()
+})
+
+bench(`l1-path-finder [11, 11] to [63, 63] with very few obstacles ${numTimesToExecute} times`, function (b) {
+  var maze = ndarray(grid, [64, 64])
+  var planner = createPlanner(maze)
+  var path = []
+  for (var i = 0; i < numTimesToExecute; i++) {
+    planner.search(11, 11, 63, 63, path)
+  }
+
+  b.end()
+})
